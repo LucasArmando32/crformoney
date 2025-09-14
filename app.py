838f8a62e2e7ce5f4803778761703537934bfb31
Lucas <reitl1@bfh.com>
2025-09-14 13:06:15 +0200

    https

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 
 TOTAL_DONATION_CHF=20
 
@@ -35,8 +38,10 @@ def donations():
 
     return render_template("donations.html", total_chf=TOTAL_DONATION_CHF)
 
-
-
+@app.before_request
+def force_https():
+    if request.headers.get('X-Forwarded-Proto', 'http') != 'https':
+        return redirect(request.url.replace('http://', 'https://', 1), code=301)
 
 
 if __name__ == "__main__":
